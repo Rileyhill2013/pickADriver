2699dd0a82bcc6d8e1251638956c0bf5c2eff22a
Riley Hill <059667@swcsd.us>
2019-11-20 14:47:58 -0500

    almost finished but working

--- pickADriver/DisplayViewController.swift
+++ pickADriver/DisplayViewController.swift
@@ -1,14 +1,14 @@
 //
-//  PeriodViewController.swift
+//  DisplayViewController.swift
 //  pickADriver
 //
-//  Created by Mason  Alexander Johnson  on 11/13/19.
+//  Created by Riley Hill on 11/20/19.
 //  Copyright © 2019 Riley Hill. All rights reserved.
 //
 
 import UIKit
 
-class PeriodViewController: UIViewController {
+class DisplayViewController: UIViewController {
     
     var period = String()
 
@@ -16,6 +16,7 @@ class PeriodViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        navigationItem.title = period
     }
     
 
@@ -23,14 +24,8 @@ class PeriodViewController: UIViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    @IBAction func onPeriodButtonTapped(_ sender: UIButton) {
-        period = (sender.titleLabel?.text)!
-        performSegue(withIdentifier: "PeriodToDisplaySegue", sender: nil)
-    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-        let dvc = segue.destination as! DisplayViewController
+        let dvc = segue.destination as! NamesTableViewController
         dvc.period = period
     }
     
